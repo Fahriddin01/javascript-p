c7b2c8ed2d65be563b3a3932ae70236f3f7fa0a4
fahriddin <fahri010793@gmail.com>
2021-11-24 14:52:11 +0500

    third

--- js/javascript.js
+++ js/javascript.js
@@ -10,12 +10,26 @@ const personalMovieDb = {
     private: false
 };
 
-const a = prompt ('Один из последных просмотренных филмов',''),
-      b = prompt ('На сколько оцените его?',''),
-      c = prompt ('Один из последных просмотренных филмов',''),
-      d = prompt ('На сколько оцените его?','');
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
 
+for (let i=0; i<2; i++) {
+    const a = prompt ('Один из последных просмотренных филмов',''),
+    b = prompt ('На сколько оцените его?','');
+    if (a !=''&& b != '' && a !=null && b !=null && a.length < 50) {
+        personalMovieDb.movies[a]=b;
+        console.log('done');
+    }else {
+     console.log('Заполните пожалуйста поля!');
+        i--;
+    }
+}      
+ if (personalMovieDb.count <10) {
+    console.log('Мало просмотра!')
+}else if ( personalMovieDb.count >=10 && personalMovieDb.count<=30){
+    console.log('Вы класический зритель');
+}else if (personalMovieDb.count >= 30 ) {
+   console.log('Вы киноман!');
+}else {
+    console.log('Произошло ошибка');
+}
 console.log(personalMovieDb);
